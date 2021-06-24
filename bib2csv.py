7256c2f278fb7cd85302b1bb68e5b7be61497d2c
MRJOANPO <jonas.powellsocialmedia@gmail.com>
2021-06-24 13:49:49 +0200

    Added support for special Characters

--- bib2csv.py
+++ bib2csv.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import pandas as pd
 from pybtex.database import parse_file
 
 def bib2csv(pBibFile, pExport):
@@ -17,13 +16,15 @@ def bib2csv(pBibFile, pExport):
 
             for author in peopleInvolved["author"]:
                 authorsText += str(author)
-                authorsText += "; "
+                if not(author == peopleInvolved["author"][-1]):
+                    authorsText += " | "
 
         if "editor" in peopleInvolved:
 
             for author in peopleInvolved["editor"]:
                 authorsText += str(author)
-                authorsText += "; "
+                if not(author == peopleInvolved["editor"][-1]):
+                    authorsText += " | "
 
         # Latex Type: book, chapter, etc.
         latexType = currentData.type
@@ -49,7 +50,21 @@ def bib2csv(pBibFile, pExport):
         entryData = [latexKey, latexType, authorsText, entryTitle, entryUrl, keywordsEntry]
         totalData.append(entryData)
 
+
     # Create Dataframe 
     columnNames  = ["Latex Key", "Latex Type", "Autoren/Editor", "Titel", "URL", "Schlagwörter"]
     df = pd.DataFrame(data=totalData, columns=columnNames)
-    df.to_csv(pExport)
+
+    # Replace all umlaute 
+    findPatterns = [r"{\\\"o}", r"{\\\"a}", r"{\\\"u}", r"{\\\"e}", r"\\~{a}", r"\\'{e}", r"\\'{u}",  r"{\\\'C}", r"{\\\'c}", r"\\\"{a}", r"{\\~a}", r"\\\"{u}", r"{\\'e}", r"{", r"}"]
+    replacePatterns = ["ö", "ä", "ü", "ë", "ã", "é", "ú", "Ć", "ć", "ä", "ã", "ü", "é", "", ""]
+    for i in range(len(findPatterns)):
+        for col in df:
+            df[col] = df[col].replace(findPatterns[i], replacePatterns[i], regex=True)
+
+    #df = df["Autoren/Editor"].str.replace(r"{\\\"o}","ö", regex=True)
+    #df.replace(findReplacePatterns, regex=True)
+    df.to_csv(pExport, sep=";", encoding="utf8")
+
+    
+    
